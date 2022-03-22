b2e906ed5d037595477d9afcabcdaf365fe42e8f
Revelt <revelt@lc-rp.mp>
2022-03-22 15:03:57 +0700

    ➕ Add CORS

--- App/api/express-init.js
+++ App/api/express-init.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = require('express')();
 const randomString = require('random-string');
-const multer = require('../config/multer-config');
 const checkApiKey = require('../config/check-api-key');
 const fileUpload = require('../config/multer-config');
+const cors = require('cors');
 
 const notifyConnect = () => {
     console.log(`SERVER API CONNECTED TO ${process.env.EXPRESS_API_HOST}:${process.env.EXPRESS_API_PORT}`);
@@ -15,7 +15,7 @@ const initiateExpress = () => {
         // eslint-disable-next-line no-console
         console.info(`Since you are not setup API key correctly, we generate it for you: ${process.env.EXPRESS_API_SECRET_KEY}`);
     }
-    
+    app.use(cors);
     app.use(express.static(__dirname + '/public'));
     app.use('/uploads', express.static('uploads'));
     app.post('/upload-single', checkApiKey, fileUpload);
